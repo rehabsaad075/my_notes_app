494e474e99a3257c42ded61674109cecf2e1cf38
Rehab Saad <saadrehab075@gmail.com>
2024-05-22 01:42:07 +0300

    create splash screen

--- lib/view/my_notes_splash_screen.dart
+++ lib/view/my_notes_splash_screen.dart
@@ -1,10 +1,19 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:my_notes_app/view/notes_home_page_screen.dart';
 
 class MyNotesSplashScreen extends StatelessWidget {
   const MyNotesSplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AnimatedSplashScreen(
+      duration: 3000,
+      splash:  ('assets/images/notes logo.png'),
+      nextScreen: const NotesHomePageScreen(),
+      splashIconSize: 170,
+      splashTransition: SplashTransition.scaleTransition,
+      backgroundColor: Colors.white,
+    );
   }
 }
