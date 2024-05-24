3e56ab1335624e925d0892a1864f9c207ed1eab4
Rehab Saad <saadrehab075@gmail.com>
2024-05-25 02:53:12 +0300

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,16 @@
+class NoteModel{
+  final String title;
+  final String content;
+  final String date;
+  final String time;
+  final int color;
+
+  NoteModel({
+    required this.title,
+    required this.content,
+    required this.date,
+    required this.time,
+    required this.color
+  });
+
+}
